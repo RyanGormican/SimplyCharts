9673a058f30ece7273c2e42d3bad135516813bb4
RyanGormican <ryan.gormican@gmail.com>
2023-12-08 02:36:32 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Icon } from '@iconify/react';
+
 function App() {
   const [chartData, setChartData] = useState(null);
-const fetchChart = async () => {
-  try {
-    const chartData = {
-      type: 'bar',
-      data: {
-        labels: [1, 2, 3],
-        datasets: [
-          {
-            label: 'Test',
-            data: [1, 2, 3],
-          },
-        ],
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Your Title Here',
+  const [showTitle, setShowTitle] = useState(true);
+  const [titleText, setTitleText] = useState('Sample Title');
+  const [primarydataLabel, setPrimaryDataLabel] = useState('Sample Data Label')
+  const fetchChart = async () => {
+    try {
+      const chartData = {
+        type: 'bar',
+        data: {
+          labels: [1, 2, 3],
+          datasets: [
+            {
+              label: primarydataLabel,
+              data: [1, 2, 3],
+            },
+          ],
         },
-      },
-    };
+        options: {
+          title: {
+            display: showTitle,
+            text: titleText,
+          },
+        },
+      };
 
-    const chartDataString = encodeURIComponent(JSON.stringify(chartData));
-    const apiUrl = `https://quickchart.io/chart?c=${chartDataString}`;
+      const chartDataString = encodeURIComponent(JSON.stringify(chartData));
+      const apiUrl = `https://quickchart.io/chart?c=${chartDataString}`;
 
-    const response = await fetch(apiUrl);
-    const blob = await response.blob();
-    const imageUrl = URL.createObjectURL(blob);
-    setChartData(imageUrl);
-  } catch (error) {
-    console.error('Error fetching chart data:', error);
-  }
-};
-
-
+      const response = await fetch(apiUrl);
+      const blob = await response.blob();
+      const imageUrl = URL.createObjectURL(blob);
+      setChartData(imageUrl);
+    } catch (error) {
+      console.error('Error fetching chart data:', error);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-       <div className="links">
+        <div className="links">
           <a href="https://www.linkedin.com/in/ryangormican/">
             <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
           </a>
@@ -52,14 +54,51 @@ const fetchChart = async () => {
             <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
           </a>
         </div>
-         <button type="button" className="btn btn-primary" onClick={fetchChart}>
-         Create Chart
-         </button>
-         {chartData && (
-          <div className="chart-container">
-            <img src={chartData} alt="Chart" />
+
+         <div>
+          <label>
+            Primary Data Label
+              <input
+                type="text"
+                value={primarydataLabel}
+                onChange={(e) => setTitleText(e.target.value)}
+              />
+        </label>
+              </div>
+
+        <div className="chart-controls">
+
+
+          <label>
+            Title
+            <input
+              type="checkbox"
+              checked={showTitle}
+              onChange={() => setShowTitle(!showTitle)}
+            />
+          </label>
+         
+          {showTitle && (
+            <label>
+             
+              <input
+                type="text"
+                value={titleText}
+                onChange={(e) => setTitleText(e.target.value)}
+              />
+            </label>
+          )}
+             <div className="button-container">
+          <button type="button" className="btn btn-primary" onClick={fetchChart}>
+            Create Chart
+          </button>
           </div>
-        )}
+          {chartData && (
+            <div className="chart-container">
+              <img src={chartData} alt="Chart" />
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
